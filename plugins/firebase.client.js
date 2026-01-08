e7264042ddfd61e7cb5415144f4d3f1c39092fcf
Yannelee <101809845+Yannelee@users.noreply.github.com>
2026-01-08 14:12:36 -0300

    Update firebase.client.js

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -5,11 +5,11 @@ export default defineNuxtPlugin(()=>{
 
   const firebaseConfig = {
     apiKey: "AIzaSyDFK1myrQmzhNmbFBeqPviZD9uEQP14zDA",
-    authDomain: "voucher-portfolio.firebaseapp.com",
-    projectId: "voucher-portfolio",
-    storageBucket: "voucher-portfolio.firebasestorage.app",
-    messagingSenderId: "809474393947",
-    appId: "1:809474393947:web:2505906556644804d97c7e"
+  authDomain: "voucher-portfolio.firebaseapp.com",
+  projectId: "voucher-portfolio",
+  storageBucket: "voucher-portfolio.firebasestorage.app",
+  messagingSenderId: "809474393947",
+  appId: "1:809474393947:web:2505906556644804d97c7e"
   };
 
   // Initialize Firebase
